7be43bd8f5ea561215c6f8ad1acf5fe374b97f65
pablomendezroyo <mendez4a@gmail.com>
2022-11-21 13:00:35 +0100

    implement stakers ui and merge gnosis

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -3,6 +3,19 @@
 # Concatenate EXTRA_OPTS string
 [[ -n "$CHECKPOINT_SYNC_URL" ]] && EXTRA_OPTS="${EXTRA_OPTS} --checkpoint-sync-url=${CHECKPOINT_SYNC_URL}"
 
+case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_GNOSIS in
+"nethermind-gnosis.dappnode.eth")
+  HTTP_ENGINE="http://nethermind-gnosis.dappnode:8551"
+  ;;
+"erigon-gnosis.dnp.dappnode.eth")
+  HTTP_ENGINE="http://erigon-gnosis.dappnode:8551"
+  ;;
+*)
+  echo "Unknown value for _DAPPNODE_GLOBAL_EXECUTION_CLIENT_GNOSIS: $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_GNOSIS"
+  HTTP_ENGINE=$_DAPPNODE_GLOBAL_EXECUTION_CLIENT_GNOSIS
+  ;;
+esac
+
 exec lighthouse \
     --debug-level $DEBUG_LEVEL \
     --network gnosis \
@@ -18,4 +31,6 @@ exec lighthouse \
     --metrics-address 0.0.0.0 \
     --metrics-port 8008 \
     --metrics-allow-origin "*" \
+    --execution-endpoint $HTTP_ENGINE \
+    --execution-jwt "/jwtsecret" \
     $EXTRA_OPTS
